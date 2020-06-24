f75687e4ff4bb2e3a6d005636db9523125270735
vermashanu <insanshanu11@gmail.com>
2020-06-24 18:18:00 -0400

    Add project files.

---
+++ COMP123_M2020_Lesson4_Part1/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP123_M2020_Lesson4_Part1
+{
+    class Person
+    {
+
+        // PRIVATE INSTANCE VARIABLES
+
+        private string m_name;
+        private int m_age;
+
+
+        // PUBLIC PROPERTIES
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                m_age = value;
+            }
+        }
+        // CONSTRUCTOR
+        Person(string name, int age)
+        {
+            m_name = name;
+            m_age = age;
+        }
+
+            // PRIVATE METHODS
+
+            // PUBLIC METHODS
+
+        }
+    }
+
---
+++ COMP123_M2020_Lesson4_Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP123_M2020_Lesson4_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
